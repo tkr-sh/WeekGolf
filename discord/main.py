ca73d930436615a883c2acc97fb768c778ba1c31
aderepas <aderepas2@gmail.com>
2022-11-03 16:03:40 +0100

    Fixing bug with discord bot

--- discord/main.py
+++ discord/main.py
@@ -459,7 +459,9 @@ def int_to_en(n: int) -> str:
 
 @client.event
 async def on_message(message):
-    global last_message, waiting_for_response, waiting_for_response_up, quiz_state, global_correct, players, players_score, players_this_round, players_correct_this_round
+    global last_message, last_message_up , waiting_for_response, waiting_for_response_up, quiz_state, global_correct, players, players_score, players_this_round, players_correct_this_round
+
+    splitable: bool = str(message.content) != ""
 
     # If it's not the bot itself
     if str(message.author) != "WeekGolf#3860" and str(message.author) != "WeekGolfBeta#2377" and str(message.guild.id) == SERVER_ID:
@@ -496,7 +498,7 @@ async def on_message(message):
 
 
         # Info
-        elif str(message.content).split()[0] == "$info":
+        elif splitable and str(message.content).split()[0] == "$info":
             # Sending the Request
             rep = requests.get("https://week.golf/description.json").json()
 
@@ -544,7 +546,7 @@ async def on_message(message):
 
 
         # Quiz
-        elif ("$quiz" in str(message.content).split() and "start" in str(message.content).split()) or str(message.content) == "$qs":
+        elif splitable and ("$quiz" in str(message.content).split() and "start" in str(message.content).split()) or str(message.content) == "$qs":
             if quiz_state == 1:
                 await message.channel.send("Game is already starting...")
             elif quiz_state == 2:
