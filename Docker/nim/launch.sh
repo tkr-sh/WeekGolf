ae07ba1786bea9b53597bc41d8e9f95d2f1ebe51
tkirishima <tkirishima@proton.me>
2023-07-13 17:34:04 +0200

    Fixing nim problem

--- Docker/nim/launch.sh
+++ Docker/nim/launch.sh
@@ -13,27 +13,29 @@ ls in
 
 cat in/prog.nim
 
+cp in/prog.nim prog.nim
+
 # Compile and get the message
-error_message="$(nim c -r in/prog.nim)"
+error_message="$(nim c -r prog.nim)"
 
 echo "$error_message"
 
 
 
 # # If there is an error
-# if echo "$error_message" | grep -q "Build FAILED."; then
+if echo "$error_message" | grep -q "Error:"; then
 
-#     # Add the message to the error files
-#     for testcount in `seq 0 1 $countNbTest`; do
-#         echo "$error_message" > /tmp/out/err$testcount.txt
-#         touch /tmp/out/out$testcount.txt
-#     done
+    # Add the message to the error files
+    for testcount in `seq 0 1 $countNbTest`; do
+        echo "$error_message" > /tmp/out/err$testcount.txt
+        touch /tmp/out/out$testcount.txt
+    done
 
-# else 
-#     for testcount in `seq 0 1 $countNbTest`; do
-#         ./prog \
-#             < /tmp/in/input$testcount.txt \
-#             > /tmp/out/out$testcount.txt  \
-#             2> /tmp/out/err$testcount.txt
-#     done
-# fi
+else 
+    for testcount in `seq 0 1 $countNbTest`; do
+        ./prog \
+            < /tmp/in/input$testcount.txt \
+            > /tmp/out/out$testcount.txt  \
+            2> /tmp/out/err$testcount.txt
+    done
+fi
