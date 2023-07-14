3d394d4089e65ddb2e83e499c205808e7502a45d
tkirishima <tkirishima@proton.me>
2023-07-14 15:05:57 +0200

    Adding sys.stdin.read(1) to python for the translate table

--- Docker/erlang/launch.sh
+++ Docker/erlang/launch.sh
@@ -7,15 +7,23 @@ cd /tmp
 # Support errors
 set +e
 
+cp in/prog.erl .
+
+# Step 2: Create an Erlang program
+echo "Creating Erlang program..."
+echo "-module(prog)." > prog.erl
+echo "-export([start/0])." >> prog.erl
+echo "start() ->" >> prog.erl
+echo "    io:format(\"Hello, World!~n\")." >> prog.erl
+
+
 # Compile
-erlc in/prog.erl
+erlc prog.erl
 
-
-ls 
 
 for testcount in `seq 0 1 $countNbTest`; do
     # erl -noshell -run prog start -s init stop \
-    erl -noshell -s prog start -s init stop \
+    erl -noshell prog \
         < /tmp/in/input$testcount.txt \
         > /tmp/out/out$testcount.txt  \
         2> /tmp/out/err$testcount.txt
